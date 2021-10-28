a51dd14a3ed4b1f74a0cbdfb0034a92b67ec1bda
Giorgetti, Valentin <valentingiorgetti@hotmail.com>
2021-10-28 11:38:31 -0300

    Defino los templates para hacer la exportación

--- lib/polycon.rb
+++ lib/polycon.rb
@@ -5,6 +5,7 @@ module Polycon
   autoload :Persistance, 'polycon/persistance'
   autoload :Helper, 'polycon/helper'
   autoload :Exceptions, 'polycon/exceptions'
+  autoload :Exporter, 'polycon/exporter'
 
   # Agregar aquí cualquier autoload que sea necesario para que se cargue las clases y
   # módulos del modelo de datos.
