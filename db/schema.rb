950b14926dadf8fb4db1e6a0792838690f1fe2f2
Giorgetti, Valentin <valentingiorgetti@hotmail.com>
2021-11-22 15:30:47 -0300

    Agrego el campo date_time a los appointments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_180909) do
+ActiveRecord::Schema.define(version: 2021_11_22_182614) do
 
   create_table "appointments", charset: "utf8", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_11_22_180909) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "professional_id"
+    t.datetime "date_time"
     t.index ["professional_id"], name: "index_appointments_on_professional_id"
   end
 
