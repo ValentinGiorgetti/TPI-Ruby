57543c2c248095bf38e8799b727dad0057ae0709
Giorgetti, Valentin <valentingiorgetti@hotmail.com>
2021-11-22 12:01:11 -0300

    Scaffold para appointments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_145542) do
+ActiveRecord::Schema.define(version: 2021_11_22_150024) do
+
+  create_table "appointments", charset: "utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "surname"
+    t.integer "phone"
+    t.string "notes"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "professionals", charset: "utf8", force: :cascade do |t|
     t.string "name"
