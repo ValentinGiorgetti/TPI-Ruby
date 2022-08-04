cb5ce16a65e082bc8f3155df643d927f335296d0
Christian Rodriguez <chrodriguez@gmail.com>
2022-08-04 18:22:18 -0300

    Correcciones iniciales

--- db/migrate/20220712143533_remove_unused_user_fields.rb
+++ db/migrate/20220712143533_remove_unused_user_fields.rb
@@ -1,9 +1,9 @@
 class RemoveUnusedUserFields < ActiveRecord::Migration[6.1]
   def change
-    remove_index :users, name: "index_users_on_reset_password_token"
-    remove_column :users, :first_name, :string
-    remove_column :users, :last_name, :string
-    remove_column :users, :reset_password_sent_at, :datetime
-    remove_column :users, :reset_password_token, :string
+#    remove_index :users, name: "index_users_on_reset_password_token"
+#    remove_column :users, :first_name, :string
+#    remove_column :users, :last_name, :string
+#    remove_column :users, :reset_password_sent_at, :datetime
+#    remove_column :users, :reset_password_token, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2022_07_12_211116) do
 
-  create_table "appointments", charset: "utf8", force: :cascade do |t|
+  create_table "appointments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "surname"
     t.integer "phone"
@@ -24,18 +24,20 @@ ActiveRecord::Schema.define(version: 2022_07_12_211116) do
     t.index ["professional_id"], name: "index_appointments_on_professional_id"
   end
 
-  create_table "professionals", charset: "utf8", force: :cascade do |t|
+  create_table "professionals", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", charset: "utf8", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.datetime "remember_created_at"
     t.string "role"
     t.string "name"
     t.string "surname"
