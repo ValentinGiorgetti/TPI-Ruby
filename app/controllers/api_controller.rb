4adb3b9572bd3f224a7d2511161519114d640408
Giorgetti, Valentin <valentingiorgetti@hotmail.com>
2022-08-09 16:39:44 -0300

    Agrego paginación en la búsqueda de appointments en la API

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,25 +1,46 @@
 class ApiController < ActionController::Base
 
-    def check_page_parameter(page)
-        (page.to_i.to_s == page) && (page.to_i >= 0)
+    def check_positive_integer(number)
+        (number.to_i.to_s == number) && (number.to_i >= 0)
+    end
+
+    def check_parameters(parameters)
+        if (parameters.empty?)
+            return false
+        end
+
+        if parameters[:page] && !check_positive_integer(parameters[:page])
+            pp 11
+            return false
+        end
+
+        if parameters[:id] && !check_positive_integer(parameters[:id])
+            return false
+        end
+
+        if (parameters.size == 1) && (!parameters[:id] && !parameters[:page])
+            return false
+        end
+
+        if parameters.size > 1 && (parameters[:id] || !parameters[:page])
+            return false
+        end
+
+        true
     end
 
     def get_professionals
         parameters = request.query_parameters
 
-        if (parameters.empty?) || (parameters.size > 2)
+        if not check_parameters(parameters)
             render json: { "description": "Bad parameters" }, status: 400 and return
         end
 
-        if (parameters.size == 1) && (!parameters[:id] && !parameters[:page])
+        if parameters.size > 2
             render json: { "description": "Bad parameters" }, status: 400 and return
         end
 
         if (parameters.size == 2) && (!(parameters[:name] && parameters[:page]))
-            render json: { "description": "Bad parameters" }, status: 400 and return
-        end
-
-        if parameters[:page] && !check_page_parameter(parameters[:page])
             render json: { "description": "Bad parameters" }, status: 400 and return
         end
 
@@ -44,22 +65,32 @@ class ApiController < ActionController::Base
     def get_appointments
         parameters = request.query_parameters
 
-        render json: Appointment.not_finished and return if parameters.empty?
-        if parameters.size > 2
+        if not check_parameters(parameters)
             render json: { "description": "Bad parameters" }, status: 400 and return
         end
 
-        if parameters.size == 1 && (!["date", "week", "id", "patient_name", "patient_surname", "professional_name"].include?(parameters.keys[0]))
+        if parameters.size > 3
             render json: { "description": "Bad parameters" }, status: 400 and return
         end
 
-        if parameters.size == 2 && (!parameters[:start_date] && !parameters[:end_date])
+        if parameters.size == 2  && (!["date", "week", "patient_name", "patient_surname", "professional_name"].include?(parameters.keys.excluding("page")[0]))
+            render json: { "description": "Bad parameters" }, status: 400 and return
+        end
+        
+        if parameters.size == 3 && (!parameters[:start_date] || !parameters[:end_date])
             render json: { "description": "Bad parameters" }, status: 400 and return
         end
 
-        render json: Appointment.where(id: parameters[:id]) and return if parameters[:id]
+        result = nil
 
-        appointments = nil
+        if parameters[:id]
+            result = Appointment.find_by(id: parameters[:id])
+            if not result
+                render json: { "description": "Not found" }, status: 404 and return
+            else 
+                render json: result and return
+            end
+        end
 
         begin
             if params[:start_date]
@@ -67,25 +98,25 @@ class ApiController < ActionController::Base
 
                 end_date = Appointment.check_date_string(params[:end_date])
 
-                appointments = Appointment.between_dates(start_date, end_date)
+                result = Appointment.between_dates(start_date, end_date)
             else 
                 if parameters[:date] || parameters[:week]
                     date = Appointment.check_date_string(params[:date] ? parameters[:date] : parameters[:week])
 
-                    appointments = params[:date] ? Appointment.all_appointments_by_date(date) : appointments = Appointment.all_appointments_by_week(date)
+                    result = params[:date] ? Appointment.all_appointments_by_date(date) : Appointment.all_appointments_by_week(date)
                 end
             end
         rescue
             render json: { "description": "Bad parameters" }, status: 400 and return
         end        
-        # "patient_name", "patient_surname", "professional_name"
-        if not appointments 
-            appointments = Appointment.find_by_patient_name(parameters[:patient_name]) if parameters[:patient_name]
-            appointments = Appointment.find_by_patient_surname(parameters[:patient_surname]) if parameters[:patient_surname] 
-            appointments = Appointment.find_by_professional_name(parameters[:professional_name]) if parameters[:professional_name]
+
+        if not result 
+            result = Appointment.find_by_patient_name(parameters[:patient_name]) if parameters[:patient_name]
+            result = Appointment.find_by_patient_surname(parameters[:patient_surname]) if parameters[:patient_surname] 
+            result = Appointment.find_by_professional_name(parameters[:professional_name]) if parameters[:professional_name]
         end
 
-        render json: appointments
+        render json: result.page(parameters[:page])
     end
 
 end
